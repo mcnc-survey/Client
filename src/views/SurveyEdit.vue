c73e7929b1d510b8205ffd8f94c25afabb346666
romi <rupi2223@gmail.com>
2024-11-13 17:34:29 +0900

    설문조사 수정 페이지 기본 구조 추가

--- src/views/SurveyEdit.vue
+++ src/views/SurveyEdit.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <h1>Login Page</h1>
-    <p>This is the Login Page!</p>
+    <h1>Edit Page</h1>
+    <p>This is the Edit Page!</p>
+    <p>편집할 설문조사의 ID: {{ $route.params.id }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'SurveyEdit'
+}
+</script>
