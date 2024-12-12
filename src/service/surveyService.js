9bc3a012b5244cac63f8d4da5448232724d4f4a7
hancy0531 <122026753+hancy0531@users.noreply.github.com>
2024-12-12 10:41:11 +0900

    Update surveyService.js
    설문 미리보기 api 추가

--- src/service/surveyService.js
+++ src/service/surveyService.js
@@ -6,19 +6,19 @@ export const surveyAPI = {
   createSurvey: (data) => axios.post("/surveys", data),
 
   // 설문 상세 조회 (수정)
-  getSurveyForEdit: (surveyId) =>
-    axios.get(`/surveys/survey-id/${surveyId}/edit`),
+  getSurveyForEdit: (surveyId) => axios.get(`/surveys/survey-id/${surveyId}/edit`),
 
   // 설문 수정
-  updateSurvey: (surveyId, data) =>
-    axios.put(`/surveys/survey-id/${surveyId}`, data),
+  updateSurvey: (surveyId, data) => axios.put(`/surveys/survey-id/${surveyId}`, data),
+
+  // 설문 미리보기 
+  getSurveyDetail: (surveyId) => axios.get(`/surveys/survey-id/${surveyId}`),
 
   // 설문 목록 조회
   getSurveys: () => axios.get(`/surveys`),
 
   // 설문 삭제 (논리적)
-  softDeleteSurvey: (surveyId) =>
-    axios.delete(`/surveys/survey-id/${surveyId}`),
+  softDeleteSurvey: (surveyId) => axios.delete(`/surveys/survey-id/${surveyId}`),
 
   // 설문 복구
   restoreSurvey: (data) => axios.post("/surveys/restore", data),
