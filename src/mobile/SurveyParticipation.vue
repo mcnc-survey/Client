5652a37545f4946a169b8976a52ab4e2a6ae47ec
bae-suho <115385697+bae-suho@users.noreply.github.com>
2024-12-16 15:43:59 +0900

    FIX: 목데이터 코드 삭제

--- src/mobile/SurveyParticipation.vue
+++ src/mobile/SurveyParticipation.vue
@@ -77,70 +77,15 @@ export default {
   methods: {
     async fetchSurveyData() {
       try {
-        // const response = await axios.get(
-        //   "https://mobile.mcnc-survey.store/responses/hello",
-        //   {
-        //     headers: {
-        //       Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        //     },
-        //   }
-        // );
-        // const surveyData = response.data.body.surveySnippet;
-
-        // 목데이터로 테스트
-        const surveyData = {
-          title: "테스트 설문 제목",
-          description: "이 설문은 테스트 목적으로 제공됩니다.",
-          question: [
-            {
-              id: "78418fcf-d2cb-41e5-a6e7-a6884b86a75e",
-              title: "현재 한국 정부에 대해 개인적인 생각을 적어주세요",
-              questionType: "LONG_ANSWER",
-              order: 1,
-              required: true,
-              etc: false,
+        const response = await axios.get(
+          "https://mobile.mcnc-survey.store/responses/hello",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
             },
-            {
-              id: 2,
-              questionType: "SINGLE_CHOICE",
-              title: "이 중에서 가장 좋아하는 색상은 무엇인가요?",
-              columns: ["빨강", "파랑", "초록", "노랑"],
-              required: true,
-            },
-            {
-              id: 3,
-              questionType: "MULTIPLE_CHOICE",
-              title: "좋아하는 과일을 모두 선택해주세요.",
-              columns: ["사과", "바나나", "딸기", "포도"],
-              required: false,
-            },
-            {
-              id: "78418fcf-d2cb-41e5-a6e7-a6884b86a75e",
-              title: "현재 한국 정부에 대해 개인적인 생각을 적어주세요",
-              questionType: "LONG_ANSWER",
-              order: 4,
-              required: true,
-              etc: false,
-            },
-            {
-              id: "d7002c5e-2020-475c-b23a-b345975034be",
-              title: "집에 가고 싶은 마음을 표현해주세요",
-              questionType: "SHORT_ANSWER",
-              order: 5,
-              required: true,
-              etc: false,
-            },
-
-            {
-              id: "d7002c5e-2020-475c-b23a-b345975034be",
-              title: "집에 가고 싶은 마음을 표현해주세요",
-              questionType: "SHORT_ANSWER",
-              order: 5,
-              required: true,
-              etc: false,
-            },
-          ],
-        };
+          }
+        );
+        const surveyData = response.data.body.surveySnippet;
 
         // 가져온 데이터를 컴포넌트 데이터에 저장
         this.survey.title = surveyData.title;
