9b1229b9d5bd138e3cec64c2337241842bbee2c7
bae-suho <115385697+bae-suho@users.noreply.github.com>
2024-12-27 11:14:20 +0900

    주석 삭제

--- src/service/mobileAxios.js
+++ src/service/mobileAxios.js
@@ -37,7 +37,6 @@ mobileInstance.interceptors.request.use(
     // 요청 보내기 전 처리
     const token = localStorage.getItem("accessToken");
     if (token) {
-      // config.headers.Authorization = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI4ZGI5N2Y1OC0zZTIwLTRkMjUtODdlZS02OGE2ZjI5NzEyOTIiLCJzdXJ2ZXlJZCI6ImhlbGxvIiwibmFtZSI6IuyImO2YuOuwsCIsImVtYWlsIjoicW90bmdoNjg1OUBnbWFpbC5jb20iLCJpYXQiOjE3MzQ5MTk2MTcsImV4cCI6MTczNDkxOTYxOH0.Umk3JLUL6p5U0EvUjDxyNzVtgQjK-ISp57ul4w8fCHXYVrQrDJBZhBvMSFtPv11RfLqR9-pJoT1iOHPGKIwL-w`;
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
